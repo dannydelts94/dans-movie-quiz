2e56b40f3e8095384c3cfc163d4850bacfdc7c46
dannydelts94 <126992132+dannydelts94@users.noreply.github.com>
2023-03-27 17:48:35 -0400

    added variables and called elements to generate first question from start page.

---
+++ script.js
@@ -0,0 +1,18 @@
+var startButton = document.getElementById('start-btn')
+var quesContainEl = document.getElementById('qcontainer')
+startButton.addEventListener('click', startQuiz)
+
+function startQuiz () {
+  console.log("started")
+  startButton.classList.add('hide')
+}
+
+function nextQues () {
+
+
+}
+
+function answerQues () {
+
+
+}
