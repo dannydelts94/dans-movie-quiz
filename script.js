746d47c95a0e93f7ada095f98053f42e444d5a6b
dannydelts94 <126992132+dannydelts94@users.noreply.github.com>
2023-03-28 00:57:34 -0400

    fixed bugs

--- script.js
+++ script.js
@@ -1,111 +1,114 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('qcontainer')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('choicebutt')
 
-var startButton = document.getElementById('start-btn')
-var nextButton = document.getElementById('next-btn')
-var quesContainEl = document.getElementById('qcontainer')
-var randomQues, currentQues
-var quesEl = document.getElementById('question')
-var answerQuesEl = document.getElementById('choicebutt')
-startButton.addEventListener('click', startQuiz)
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () => {
-    currentQues++
-    nextQues()
+  currentQuestionIndex++
+  setNextQuestion()
 })
 function startTimer() {
-    var timeLeft = 60;
-    var timer = setInterval(function() {
-      if (timeLeft <= 0) {
-        clearInterval(timer);
-        document.getElementById("timer").innerHTML = "Time's up!";
-      } else {
-        document.getElementById("timer").innerHTML = timeLeft + " seconds remaining";
-      }
-      timeLeft -= 1;
-    }, 1000);
-  }
-  
-
-function startQuiz () {
+  var timeLeft = 60;
+  var timer = setInterval(function () {
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+      document.getElementById("timer").innerHTML = "GAME OVER!";
+      questionContainerElement.classList.add('hide')
+    } else {
+      document.getElementById("timer").innerHTML = timeLeft + " seconds remaining";
+    }
+    timeLeft -= 1;
+  }, 1000);
+}
+function startGame() {
   startButton.classList.add('hide')
-  randomQues = questions.sort(() => Math.random() - .5)
-  currentQues = 0
-  quesContainEl.classList.remove('hide')
-  nextQues()
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
 }
 
-function nextQues () {
-    resetQues ()
-    showQuestion(randomQues[currentQues])
-
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
+
 function showQuestion(question) {
-    quesEl.innerText = question.question
-    question.answers.forEach(answer => {
-      var aButton = document.createElement('aButton')
-      aButton.innerText = answer.text
-      aButton.classList.add('btn')
-      if (answer.correct)  {
-        aButton.dataset.correct = answer.correct
-      }
-      aButton.addEventListener('click', answerQues)
-      answerQuesEl.appendChild(aButton)
-    });
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
 }
-function resetQues() {
-    clearStatusClass(document.body)
-    nextButton.classList.add('hide')
-    while (answerQuesEl.firstChild) {
-        answerQuesEl.removeChild
-        (answerQuesEl.firstChild)
-      
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
 }
-}
-function answerQues(e) {
-    var answeredQues = e.target
-    var correct = answeredQues.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerQuesEl.children).forEach(aButton => {
-    setStatusClass(aButton, aButton.dataset.correct)
-    } )
-    if (randomQues.legnth > currentQues + 1) {
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
     nextButton.classList.remove('hide')
-} else {
+  } else {
     startButton.innerText = 'Restart'
     startButton.classList.remove('hide')
+  }
 }
-}
+
 function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add('correct')
-    } else {
-        element.classList.add('incorrect')
-    }
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
 }
 
 function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('incorrect')
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
 }
+
+
 var questions = [
-{
- question: 'Who starred in Black Swan?',
- answers: [
-    { text: 'Natalie Portman', correct: true},
-    { text: 'Blake Lively', incorrect: false},
-    { text: 'Angelina Jolie', incorrect: false},
-    { text: 'Pamela Anderson', incorrect: false},
- ]
-},
-{
- question: 'Who was known as the Master of Suspense?',
- answers: [
-    { text: 'Martin Scorcese', incorrect: false},
-    { text: 'Steven Spielberg', incorrect: false},
-    { text: 'Alfred Hitchcock', correct: true},
-    { text: 'Fritz Lang', incorrect: false},
- ]
-}
+  {
+    question: 'Who starred in Black Swan?',
+    answers: [
+      { text: 'Natalie Portman', correct: true },
+      { text: 'Blake Lively', incorrect: false },
+      { text: 'Angelina Jolie', incorrect: false },
+      { text: 'Pamela Anderson', incorrect: false },
+    ]
+  },
+  {
+    question: 'Who was known as the Master of Suspense?',
+    answers: [
+      { text: 'Martin Scorcese', incorrect: false },
+      { text: 'Steven Spielberg', incorrect: false },
+      { text: 'Alfred Hitchcock', correct: true },
+      { text: 'Fritz Lang', incorrect: false },
+    ]
+  }
 
 
 
- ]
+]
