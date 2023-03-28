d6ffed5d5afe6b3be08c7b048f754164394fb3e8
dannydelts94 <126992132+dannydelts94@users.noreply.github.com>
2023-03-27 20:24:47 -0400

    added additional questions to quiz.

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ var randomQues, currentQues
 var quesEl = document.getElementById('question')
 var answerQuesEl = document.getElementById('choicebutt')
 startButton.addEventListener('click', startQuiz)
+nextButton.addEventListener('click', () => {
+    currentQues++
+    nextQues()
+})
+
 
 function startQuiz () {
   startButton.classList.add('hide')
@@ -33,22 +38,58 @@ function showQuestion(question) {
     });
 }
 function resetQues() {
+    clearStatusClass(document.body)
     nextButton.classList.add('hide')
     while (answerQuesEl.firstChild) {
-        answerQuesEl.removeChild(answerQuesEl.firstChild)
+        answerQuesEl.removeChild
+        (answerQuesEl.firstChild)
+      
+}
+}
+function answerQues(e) {
+    var answeredQues = e.target
+    var correct = answeredQues.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerQuesEl.children).forEach(aButton => {
+    setStatusClass(aButton, aButton.dataset.correct)
+    } )
+    if (randomQues.legnth > currentQues + 1) {
+    nextButton.classList.remove('hide')
+} else {
+    startButton.innerText - 'Restart'
+    startButton.classList.remove('hide')
+}
+}
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('incorrect')
     }
 }
-function answerQues () {
 
-
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('incorrect')
 }
-
 var questions = [
 {
  question: 'Who starred in Black Swan?',
  answers: [
     { text: 'Natalie Portman', correct: true},
-    { text: 'Blake Lively', correct: false},
+    { text: 'Blake Lively', incorrect: false},
+    { text: 'Angelina Jolie', incorrect: false},
+    { text: 'Pamela Anderson', incorrect: false},
+ ]
+},
+{
+ question: 'Who was known as the Master of Suspense?',
+ answers: [
+    { text: 'Martin Scorcese', incorrect: false},
+    { text: 'Steven Spielberg', incorrect: false},
+    { text: 'Alfred Hitchcock', correct: true},
+    { text: 'Fritz Lang', incorrect: false},
  ]
 }
 
